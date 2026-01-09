0e2e9e02cd1a6080ce99d6c78fe3e8f98929aeb7
Claudio Barlassina <claudio_barlassina_ar@hotmail.com>
2026-01-09 02:58:27 -0300

    limpieza

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,20 @@
-
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import { useEffect } from 'react'
-import Details from './Components/Details'
-import {  Routes, Router, Route } from 'react-router-dom'
+
+import { Routes, Router, Route } from 'react-router-dom'
 import Home from './Components/Home'
-
-
-
 
 function App() {
   useEffect(() => {
     AOS.init({ duration: 1000 })
   }, [])
 
-
-
-
   return (
     <>
-
-    
-      {/* <Navbar></Navbar>
-      <Hero></Hero>
-      {/* <AutoPlay></AutoPlay> */}
-      {/* <About></About>
-      <Proyects></Proyects> 
-      <Contacts></Contacts> */} 
-
-   
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
-        
       </Routes>
-
- 
-
-
-
     </>
   )
 }
